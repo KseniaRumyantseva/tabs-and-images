99a824a674ad6d2425127d4b5362528c7c61dc31
KseniaRumyantseva <aratisnotbad@yandex.ru>
2021-11-02 18:07:02 +0500

    update d-scrict.js

--- d-script.js
+++ d-script.js
@@ -2,59 +2,62 @@ let tabs =[
     {
         header: 'Водопад Кон',
         body: '<img src="images/vodopad_kon-500x333.jpg" alt="">',
-        target: 'tab-1'
     },
     {
         header: 'Водопад Игуасу',
         body: '<img src="images/vodopad_iguasu.jpg" alt="">',
-        target: 'tab-2'
     },
     {
         header: 'Ниагарский Водопад',
         body: '<img src="images/vodopad_niagarskiy-500x313.jpg" alt="">',
-        target: 'tab-3'
     },
     {
         header: 'Водопад Виктория',
         body: '<img src="images/vodopad_viktoria.jpg" alt="">',
-        target: 'tab-4'
     },
     {
         header: 'Водопад Анхель',
         body: '<img src="images/vodopad_anhel.jpg" alt="">',
-        target: 'tab-5'
     }
 ]
 
 const tabHeader = document.querySelector('.tab-header');
-const tabBody = document.querySelector('.tab-body');
+const tabBodyItem = document.querySelector('.tab-body__item');
 
+let i = 0;
 tabs.forEach(tab => {
-    let divHeader = document.createElement('div');
-    divHeader.classList.add('tab-header__item')
-    divHeader.dataset.target = tab.target
-    divHeader.innerHTML=`<span>${tab.header}</span>`
-    divHeader.addEventListener('click', doActiveTab)
-
-    let divBody = document.createElement('div')
-    divBody.classList.add('tab-body__item')
-    divBody.innerHTML=`<div>${tab.body}</div>`
-    divBody.id = tab.target
+    let divHeader = createElement('div', ['tab-header__item'], `<span>${tab.header}</span>`);
+    divHeader.dataset.target = i;
+    divHeader.addEventListener('click', doActiveTab);
 
     tabHeader.append(divHeader)
-    tabBody.append(divBody)
+
+    i++;
 })
+
+function createElement(el, classes, template){
+    let element = document.createElement(el);
+    element.classList.add(...classes)
+    element.innerHTML = template;
+    return element;
+}
+
+start()
+
+function start() {
+    tabHeader.firstChild.classList.add('active');
+    tabBodyItem.innerHTML = `<p>${tabs[0].body}</p>`
+}
 
 function  doActiveTab(e){
     clearTab()
     let currentHeader = e.target.closest('div.tab-header__item')
     currentHeader.classList.add('active')
 
-    let currentBody = document.querySelector(`#${currentHeader.dataset.target}`)
-    currentBody.classList.add('active')
+    let currentTab = tabs[currentHeader.dataset.target];
+    tabBodyItem.innerHTML = `<p>${currentTab.body}</p>`;
 }
 
 function clearTab(){
     [...tabHeader.children].forEach(item => item.classList.remove('active'));
-    [...tabBody.children].forEach(item => item.classList.remove('active'));
 }
